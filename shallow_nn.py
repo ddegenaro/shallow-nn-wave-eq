94b8dd9ebaafef6575089f51529e46148eca798a
Dan DeGenaro <daniel.degenaro@gmail.com>
2025-11-04 21:57:03 -0500

    enforcing some constraints

--- shallow_nn.py
+++ shallow_nn.py
@@ -1,31 +1,33 @@
 import torch
 from torch import nn
 
-class Wave2D(nn.Module):
+class Wave3D(nn.Module):
 
-    def __init__(self, width: int = 10):
+    def __init__(self, width: int = 10, c: float = 1.):
         super().__init__()
-        self.network = nn.Sequential(
-            nn.Linear(4, width),
-            nn.ReLU(),
-            nn.Linear(width, 1)
-        )
+
+        self.position_weights = nn.Parameter(torch.randn((3, width)))
+        self.output_weights = nn.Linear(width, 1)
+        self.c = c
         
 
-    def forward(self, t, x, y, z):
-        return self.network(
-            torch.transpose(
-                torch.vstack([t, x, y, z]), 0, 1
-            )
-        )
+    def forward(self, t, x, y, z) -> torch.Tensor:
+
+        pos_out = torch.hstack([x, y, z]) @ self.position_weights
+
+        time_out = t @ torch.sqrt(self.c*(self.position_weights**2).sum(0, keepdim=True))
+
+        return self.output_weights(time_out + pos_out)
+    
+    def u(self, t, x, y, z) -> torch.Tensor:
+        return self.forward(t, x, y, z)
     
 if __name__ == "__main__":
-    net = Wave2D(10)
+    net = Wave3D(10)
 
-    # randomly init, but "predicting" displacement at the origin for 10 time units
-    print(net(
-        torch.tensor([i for i in range(10)], dtype=torch.float32),
-        torch.tensor([0] * 10, dtype=torch.float32),
-        torch.tensor([0] * 10, dtype=torch.float32),
-        torch.tensor([0] * 10, dtype=torch.float32)
-    ))
+    # predict at some random times/places
+    t, x, y, z = torch.randn((32, 1)), torch.randn((32, 1)), torch.randn((32, 1)), torch.randn((32, 1))
+
+    out = net.u(t, x, y ,z)
+
+    print(out.shape)
